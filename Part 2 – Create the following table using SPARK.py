d4b8ef38155e6d5b1ab91e3769a6ded124fdf56b
Zhen XING <zhenxing.scut@aiesec.net>
2021-01-18 10:10:23 +0100

    Create Part 2 – Create the following table using SPARK.py

---
+++ Part 2 – Create the following table using SPARK.py
@@ -0,0 +1,27 @@
+!pip install pyspark
+from datascroller import scroll
+from pyspark import SparkContext, SQLContext
+from pyspark.sql import Row
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from pyspark.sql.functions import col,udf
+@udf(returnType='int')
+def cnt(word):
+  L=[]                           #create an empty list
+  for letter in word:
+      if letter not in L:
+          L.append(letter)       #append unique chars to list
+
+  return len(L)                   #count the chars in list
+
+@udf(returnType='boolean')
+def check(word):
+  if  "rue de Paris" in word:
+    return True
+  else:
+    return False
+
+
+transactionDataFrameTemp=transactionDataFrame.withColumn('nb_distinct_char_in_address', cnt(col("address"))).withColumn('ls_located_in_rue_de_paris', check(col("address")))
+transactionDataFrameTemp.show()
+
