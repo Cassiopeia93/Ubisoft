49f6030a131073e04a769dd6da59e07ca215c966
Zhen XING <zhenxing.scut@aiesec.net>
2021-01-19 17:26:27 +0100

    Update Part 3 – Create the following table using either SPARK or HIVE.py

--- Part 3 – Create the following table using either SPARK or HIVE.py
+++ Part 3 – Create the following table using either SPARK or HIVE.py
@@ -1,20 +1,17 @@
 
 #Generate a column which gives the total amount spent per customer in the last 10 minutes (without including the current transaction).
 
-!pip install pyspark
-from pyspark import SparkContext, SQLContext
-from pyspark.sql import Row
-from pyspark.sql import SQLContext
-from pyspark.sql.types import *
-import pyspark.sql.functions as psf
 from pyspark.sql import Window
-from pyspark.sql import functions as f
-sqlContext.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
+import pyspark.sql.functions as F 
 
-#transactionDataFrame.withColumn('sumres', sum(col("address"))).over(w).rowsBetween(-3, -1)
-transactionDataFrame=transactionDataFrame.withColumn("current_date",psf.unix_timestamp(psf.current_date()))
-transactionDataFrame=transactionDataFrame.withColumn('datetime', psf.unix_timestamp(psf.col('order_datetime'))).withColumn('10min_delta', (psf.col('current_date') - psf.lag('datetime').over(w)) / 60 <= 10) 
+df = schemaTransaction.withColumn('timestamp',schemaTransaction.order_datetime.astype('Timestamp').cast("long"))
+#df.show()
 
-result=transactionDataFrame.withColumn('amount_spent_last_ten_minutes', f.sum('amount_eur').over(Window.partitionBy('customerid').orderBy('order_datetime').rowsBetween(-3, -1)))
-result=result.where(f.col("10min_delta").isin({True}))
-result.orderBy('customerid').show()
+exclude_current = F.udf(lambda lst: lst[:-1] if lst[:-1] else [0])
+
+windowSpec = Window.partitionBy("customerid").orderBy("timestamp").rangeBetween(-600, 0)
+df1 =(
+    df.withColumn("last_10_Min", exclude_current(F.collect_list("amount_eur").over(windowSpec)))
+)
+
+df1.collect()
