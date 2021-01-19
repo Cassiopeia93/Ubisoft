497e7f2f8ba2802576b6b2c55496c2ed8f9c75ee
Zhen XING <zhenxing.scut@aiesec.net>
2021-01-19 15:03:44 +0100

    Update Part 2 – Create the following table using SPARK.py

--- Part 2 – Create the following table using SPARK.py
+++ Part 2 – Create the following table using SPARK.py
@@ -2,8 +2,6 @@
 # Generate a column which flags “True” if the address is located in “rue de Paris”.
 
 
-!pip install pyspark
-from datascroller import scroll
 from pyspark import SparkContext, SQLContext
 from pyspark.sql import Row
 from pyspark.sql import SQLContext
@@ -13,7 +11,7 @@ from pyspark.sql.functions import col,udf
 def cnt(word):
   L=[]                           #create an empty list
   for letter in word:
-      if letter not in L:
+      if letter not in L and letter != ' ':
           L.append(letter)       #append unique chars to list
 
   return len(L)                   #count the chars in list
@@ -26,6 +24,5 @@ def check(word):
     return False
 
 
-transactionDataFrameTemp=transactionDataFrame.withColumn('nb_distinct_char_in_address', cnt(col("address"))).withColumn('ls_located_in_rue_de_paris', check(col("address")))
-transactionDataFrameTemp.show()
-
+transactionDataFrameTemp=schemaTransaction.withColumn('nb_distinct_char_in_address', cnt(col("address"))).withColumn('ls_located_in_rue_de_paris', check(col("address")))
+transactionDataFrameTemp.collect()
